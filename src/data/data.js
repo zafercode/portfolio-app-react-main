4d13a230c2fcfcc31cffacae32e74302e0e77772
Zafer Kalik <68650065+Zafer720@users.noreply.github.com>
2023-01-03 03:45:41 +0300

    Update

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+import GroceryStore from '../assets/projects/grocery-store.jpg'
+import RestaurantApp from '../assets/projects/restaurant.jpg'
+import SocialMediaApp from '../assets/projects/socialMediaApp.jpg'
+import Coffee from '../assets/projects/coffee_sales.jpg'
+import WeatherApp from '../assets/projects/havaDurumuApp.jpg'
+import NetflixClone from '../assets/projects/Netflix-clone.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"There is a Netflix Clone. TailwindCss and React Js libraries were used while developing.",
+        image:NetflixClone,
+        github:"https://github.com/zafercode/netflix-clone-react-firebase-TailwindCss-",
+        live:"",
+    },
+    {
+        id:2,
+        name:"This is a classic social media app. Similar to Facebook ",
+        image:SocialMediaApp,
+        github:"https://github.com/zafercode/ReactJs-SocialMedia-App-Without-Backend",
+        live:"https://socialmedia-applicationn.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Restaurant Application.You can order and add to cart. ",
+        image:RestaurantApp,
+        github:"https://github.com/zafercode/responsive-restaurant-website",
+        live:"https://responsive-restaurant-websitee.netlify.app/",
+    },
+    {
+        id:4,
+        name:"This is a coffee shop app. Developed with vanilla JavaScript.",
+        image:Coffee,
+        github:"https://github.com/zafercode/Responsive-coffeeshop-website",
+        live:"https://coffe-shop-responsive.netlify.app/",
+    },
+    {
+        id:5,
+        name:"Weather app. Data was retrieved with API.",
+        image:WeatherApp,
+        github:"https://github.com/zafercode/Weather-App-ReactJs",
+        live:"https://hava-durumu-app-react.netlify.app/",
+    },
+    {
+        id:6,
+        name:"It's a grocery store web page",
+        image:GroceryStore,
+        github:"https://github.com/zafercode/Responsive-GroceryStore-Website",
+        live:"https://groceryy.netlify.app/",
+    }
+    
+]
