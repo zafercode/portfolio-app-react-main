985b8b9faba7eb36ed9c2dcf1cc9471db4bc44a0
zafer <zaferkalik@gmail.com>
2023-04-26 10:07:31 +0300

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,66 +3,73 @@ import Swal from "sweetalert2";
 
 class Contact extends Component {
   state = {
-    name: '',
-    email: '',
-    phone: '',
-    subject: '',
-    message: '',
-    emailStatus: ''
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
+    emailStatus: "",
   };
 
   // handle the value
-  handleChange = input => e => {
+  handleChange = (input) => (e) => {
     this.setState({ [input]: e.target.value });
-  }
+  };
 
   // when submit btn is clicked
   submitForm = (e) => {
     const { name, email, phone, subject, message } = this.state;
 
-
     // create a new XMLHttpRequest
     var xhr = new XMLHttpRequest();
 
     // get a callback when the server responds
-    xhr.addEventListener('load', () => {
+    xhr.addEventListener("load", () => {
       // update the response state and the step
 
       this.setState({
-        emailStatus: xhr.responseText
+        emailStatus: xhr.responseText,
       });
     });
     // open the request with the verb and the url
-    xhr.open('GET', 'https://zaferkalik.com/mail-api-react-active/contact-mail-api.php?email=' + email +
-      '&name-surname=' + name +
-      '&phone=' + phone +
-      '&subject=' + subject +
-      '&message=' + message);
+    xhr.open(
+      "GET",
+      "https://zaferkalik.com/mail-api-react-active/contact-mail-api.php?email=" +
+        email +
+        "&name-surname=" +
+        name +
+        "&phone=" +
+        phone +
+        "&subject=" +
+        subject +
+        "&message=" +
+        message
+    );
     // send the request
     xhr.send();
 
     // reset the fields
     this.setState({
-      name: '',
-      email: '',
-      phone: '',
-      subject: '',
-      message: ''
+      name: "",
+      email: "",
+      phone: "",
+      subject: "",
+      message: "",
     });
     e.preventDefault();
-  }
+  };
 
   render() {
     const { name, email, phone, subject, message, emailStatus } = this.state;
 
     const Alert = () => {
       Swal.fire({
-        icon: 'success',
-        title: 'Mesajınız Gönderildi.',
-        text: 'Yakında Sizinle İletişime Geçeceğiz.',
-        confirmButtonText: 'Tamam'
-      })
-    }
+        icon: "success",
+        title: "Mesajınız Gönderildi.",
+        text: "Yakında Sizinle İletişime Geçeceğiz.",
+        confirmButtonText: "Tamam",
+      });
+    };
 
     return (
       <div className="formBlock" onSubmit={this.submitForm}>
@@ -82,12 +89,13 @@ class Contact extends Component {
                 Contact
               </p>
               <p className="text-gray-300 py-4">
-            // Submit the form below or shoot me an email - zaferkalik@gmail.com
+                // Submit the form below or shoot me an email -
+                zaferkalik@gmail.com
               </p>
             </div>
             <input
               value={name}
-              onChange={this.handleChange('name')}
+              onChange={this.handleChange("name")}
               className="bg-[#ccd6f6] p-2"
               type="text"
               placeholder="Name"
@@ -96,7 +104,7 @@ class Contact extends Component {
             />
             <input
               value={subject}
-              onChange={this.handleChange('subject')}
+              onChange={this.handleChange("subject")}
               className="mt-4 p-2 bg-[#ccd6f6]"
               type="subject"
               placeholder="Subject"
@@ -106,7 +114,7 @@ class Contact extends Component {
 
             <input
               value={email}
-              onChange={this.handleChange('email')}
+              onChange={this.handleChange("email")}
               className="mt-4 p-2 bg-[#ccd6f6]"
               type="email"
               placeholder="Email"
@@ -115,7 +123,7 @@ class Contact extends Component {
             />
             <input
               value={phone}
-              onChange={this.handleChange('phone')}
+              onChange={this.handleChange("phone")}
               className="my-4 p-2 bg-[#ccd6f6] outline-none"
               type="number"
               placeholder="Phone number"
@@ -124,21 +132,24 @@ class Contact extends Component {
             />
             <textarea
               value={message}
-              onChange={this.handleChange('message')}
+              onChange={this.handleChange("message")}
               className="bg-[#ccd6f6] p-2"
               name="message"
               rows="10"
               placeholder="Message"
               required
             ></textarea>
-            <button type="submit" className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+            <button
+              type="submit"
+              className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
+            >
               Let's Contact
             </button>
           </form>
         </div>
       </div>
     );
-  };
+  }
 }
 
 export default Contact;
